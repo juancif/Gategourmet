4c70631ef816166f7da2be214d3faece1553ef90
juancif <cifuenteslozanojuandavid@gmail.com>
2024-06-27 15:36:29 -0500

    Correccion errores adicionales

--- register/register_admin.php
+++ register/register_admin.php
@@ -1,6 +1,6 @@
 
 <?php
-include_once("config_register.php3.php");
+include_once("config_register.php");
 if(isset($_POST['Submit'])) {
 $nombre_usuario = $_POST['nombre_usuario'];
 $contrasena = $_POST['contrasena'];
