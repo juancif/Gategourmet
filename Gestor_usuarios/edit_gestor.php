4ee649996005d07b7dca728924dde9777cd3d74d
juancif <cifuenteslozanojuandavid@gmail.com>
2024-07-05 09:09:59 -0500

    Correccion gestor de usuarios con nuevos parametros de la tabla usuarios

--- Gestor_usuarios/edit_gestor.php
+++ Gestor_usuarios/edit_gestor.php
@@ -6,15 +6,13 @@ if(isset($_POST['update']))
     $nombre_usuario = $_POST['nombre_usuario'];
     $contrasena = $_POST['contrasena'];
     $correo = $_POST['correo'];
-    $nombres = $_POST['nombres'];
-    $apellidos = $_POST['apellidos'];
-    $tipo_documento = $_POST['tipo_documento'];
+    $nombres_apellidos = $_POST['nombres_apellidos'];;
     $documento = $_POST['documento'];
     $area = $_POST['area'];
-    $tipo_usuario = $_POST['tipo_usuario'];
+    $cargo = $_POST['cargo'];
 
-    if(empty($nombre_usuario) || empty($contrasena) || empty($correo) || empty($nombres) || empty($apellidos) 
-    || empty($tipo_documento) || empty($documento) || empty($area) || empty($tipo_usuario)) {
+    if(empty($nombre_usuario) || empty($contrasena) || empty($correo) || empty($nombres_apellidos) || 
+    empty($documento) || empty($area) || empty($cargo)) {
         if(empty($nombre_usuario)) {
             echo "<font color='red'>Campo: nombre_usuario está vacío.</font><br/>";
         }
@@ -24,14 +22,8 @@ if(isset($_POST['update']))
         if(empty($correo)) {
             echo "<font color='red'>Campo: correo está vacío.</font><br/>";
         }
-        if(empty($nombres)) {
-            echo "<font color='red'>Campo: nombres está vacío.</font><br/>";
-        }
-        if(empty($apellidos)) {
-            echo "<font color='red'>Campo: apellidos está vacío.</font><br/>";
-        }
-        if(empty($tipo_documento)) {
-            echo "<font color='red'>Campo: tipo_documento está vacío.</font><br/>";
+        if(empty($nombres_apellidos)) {
+            echo "<font color='red'>Campo: nombres_apellidos está vacío.</font><br/>";
         }
         if(empty($documento)) {
             echo "<font color='red'>Campo: documento está vacío.</font><br/>";
@@ -39,23 +31,21 @@ if(isset($_POST['update']))
         if(empty($area)) {
             echo "<font color='red'>Campo: area está vacío.</font><br/>";
         }
-        if(empty($tipo_usuario)) {
-            echo "<font color='red'>Campo: tipo_usuario está vacío.</font><br/>";
+        if(empty($cargo)) {
+            echo "<font color='red'>Campo: cargo está vacío.</font><br/>";
         }
     } else {
-        $sql = "UPDATE usuarios SET nombre_usuario=:nombre_usuario, contrasena=:contrasena, correo=:correo, nombres=:nombres, 
-        apellidos=:apellidos, tipo_documento=:tipo_documento, documento=:documento, area=:area, tipo_usuario=:tipo_usuario 
+        $sql = "UPDATE usuarios SET nombre_usuario=:nombre_usuario, contrasena=:contrasena, correo=:correo, nombres_apellidos=:nombres_apellidos, 
+       documento=:documento, area=:area, cargo=:cargo 
         WHERE documento=:documento";
         $query = $dbConn->prepare($sql);
         $query->bindParam(':nombre_usuario', $nombre_usuario);
         $query->bindParam(':contrasena', $contrasena);
         $query->bindParam(':correo', $correo);
-        $query->bindParam(':nombres', $nombres);
-        $query->bindParam(':apellidos', $apellidos);
-        $query->bindParam(':tipo_documento', $tipo_documento);
+        $query->bindParam(':nombres_apellidos', $nombres_apellidos);
         $query->bindParam(':documento', $documento);
         $query->bindParam(':area', $area);
-        $query->bindParam(':tipo_usuario', $tipo_usuario);
+        $query->bindParam(':cargo', $cargo);
         $query->execute();
         header("Location: index_gestor.php");
     }
@@ -71,12 +61,10 @@ $row = $query->fetch(PDO::FETCH_ASSOC);
 $nombre_usuario = $row['nombre_usuario'];
 $contrasena = $row['contrasena'];
 $correo = $row['correo'];
-$nombres = $row['nombres'];
-$apellidos = $row['apellidos'];
-$tipo_documento = $row['tipo_documento'];
+$nombres_apellidos = $row['nombres_apellidos'];
 $documento = $row['documento'];
 $area = $row['area'];
-$tipo_usuario = $row['tipo_usuario'];
+$cargo = $row['cargo'];
 ?>
 
 <html>
@@ -109,35 +97,20 @@ $tipo_usuario = $row['tipo_usuario'];
                         <input type="email" id="correo" name="correo" required value="<?php echo $correo;?>">
                     </div>
                     <div class="input-group">
-                        <label for="first_name">Nombres</label>
-                        <input type="text" id="nombres" name="nombres" required value="<?php echo $nombres;?>">
-                    </div>
-                    <div class="input-group">
-                        <label for="last_name">Apellidos</label>
-                        <input type="text" id="apellidos" name="apellidos" required value="<?php echo $apellidos;?>">
-                    </div>
-                    <div class="input-group">
-                        <label for="document_type">Tipo de Documento</label>
-                        <select id="tipo_documento" name="tipo_documento" required value="<?php echo $tipo_documento;?>"> 
-                            <option value="C.C">Cédula de Ciudadanía</option>
-                            <option value="C.E">Cédula de Extranjería</option>
-                        </select>
+                        <label for="first_name">nombres_apellidos</label>
+                        <input type="text" id="nombres_apellidos" name="nombres_apellidos" required value="<?php echo $nombres_apellidos;?>">
                     </div>
                     <div class="input-group">
                         <label for="document">Documento</label>
                         <input type="text" id="documento" name="documento" required value="<?php echo $documento;?>">
                     </div>
                     <div class="input-group">
-                        <label for="nombres">Área Pertenece</label>
+                        <label for="nombres_apellidos">Área Pertenece</label>
                         <input type="text" id="area" name="area" value="<?php echo $area;?>">
                     </div>
                     <div class="input-group">
                         <label for="user_type">Tipo de Usuario</label>
-                        <select id="tipo_usuario" name="tipo_usuario" required value="<?php echo $tipo_usuario?>">
-                            <option value="Administrativo">Administrativo</option>
-                            <option value="Operativo">Operativo</option>
-                            <option value="Jefe de area">Jefe De Área</option>
-                        </select>
+                        <input type="tex" id="cargo" name="cargo" value="<?php echo $cargo;?>">
                     </div>
                     <div class="buttons">
                     <input type="Submit" name="update" value="Editar" class="Registrarse"></input>
