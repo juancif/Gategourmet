aecce24dfb717ce87cce45ab191aa89519662d37
juancif <cifuenteslozanojuandavid@gmail.com>
2024-08-05 13:34:32 -0500

    Se agregan opciones multiples al logn3.php y se agrega la estrucutra de las tabla de usuarios y roles

--- register/register3.php
+++ register/register3.php
@@ -116,16 +116,72 @@ if (isset($_POST['Submit'])) {
                     </div>
                     <div class="input-group">
                         <label for="area">Área</label>
-                        <input type="text" id="area" name="area" required>
+                        <select name="cargo" id="cargo">
+                            <option value="">Seleccione una opción</option>
+                            <option value="Gestion_corporativa">Gestión corporativa</option>
+                            <option value="Compliance">Compliance</option>
+                            <option value="Supply_chain">Supply Chain</option>
+                            <option value="Culinary Excellence">Culinary Excellence</option>
+                            <option value="Supervisor"  >Service Delivery</option>
+                            <option value="Assembly">Assembly</option>
+                            <option value="Servicios_institucionales">Servicios institucionales</option>
+                            <option value="Financiera">Financiera</option>
+                            <option value="Costos">Costos</option>
+                            <option value="Comunicaciones">Comunicaciones</option>
+                            <option value="Tecnologia_de_la_información">Tecnologia de la información</option>
+                            <option value="Talento_humano">Talento Humano</option>
+                            <option value="Mateninimiento">Mateninimiento</option>
+                            <option value="Servicio_al_cliente">Servicio al cliente</option>
+                            <option value="Security">Security</option>
+                        </select>
                     </div>
                     <div class="input-group">
-                        <label for="cargo">Cargo</label>
+                        <label for="area">Cargo</label>
+                        <select name="cargo" id="cargo">
+                            <option value="">Seleccione una opción</option>
+                            <option value="Auxiliar_Contable">Auxiliar Contable</option>
+                            <option value="Continuous_Improvement_Manager">Continuous Improvement Manager</option>
+                            <option value="Coordinador_de_mejoramiento_Continuo">Coordinador de mejoramiento Continuo</option>
+                            <option value="Country_Manager">Country Manager</option>
+                            <option value="CPC_Champion">CPC Champion</option>
+                            <option value="Director_Comercial">Director Comercial</option>
+                            <option value="EHS_Manager">EHS Manager</option>
+                            <option value="Especialista_de_Seguridad_en_Rampa">Especialista de Seguridad en Rampa</option>
+                            <option value="Especialista_en_Mantenimiento">Especialista en Mantenimiento</option>
+                            <option value="Executive_Sous_Chef">Executive Sous Chef</option>
+                            <option value="Jefe_Control_de_Riesgos_Fisicos">Jefe Control de Riesgos Fisicos</option>
+                            <option value="Jefe_de_Costos">Jefe de Costos</option>
+                            <option value="Junior_Section_Manager_OP&D">Junior Section Manager OP&D</option>
+                            <option value="Junior_Key_Account_Officer">Junior Key Account Officer</option>
+                            <option value="Manager_HR">Manager HR</option>
+                            <option value="Manager_Ordering">Manager, Ordering</option>
+                            <option value="Manager_Transport">Manager Transport</option>
+                            <option value="Manager_New_Operations">Manager New Operations</option>
+                            <option value="Manager_Finance">Manager Finance </option>
+                            <option value="Process_Owner_Assembly">Process Owner Assembly</option>
+                            <option value="Process_Owner_Planning_&_Supply_Chain">Process Owner Planning & Supply Chain</option>
+                            <option value="Process_Owner_Service_Delivery">Process Owner Service Delivery</option>
+                            <option value="Section_Manager_Pick_&_Pack">Section_Manager_Pick_&_Pack</option>
+                            <option value="Section_Manager_Wash_&_Pack">Section Manager Wash & Pack</option>
+                            <option value="Section_Manager_Laundry">Section Manager Laundry</option>
+                            <option value="Section_Manager_Make_&_Pack">Section Manager Make & Pack</option>
+                            <option value="Section_Manager_IDS">Section Manager - IDS</option>
+                            <option value="Sous_Chef">Sous Chef</option>
+                            <option value="Senior_Manager_Facility_Services">Senior Manager Facility Services</option>
+                            <option value="Superintendent_HR">Superintendent HR</option>
+                            <option value="Superintendent_Development_And_Communications">Superintendent Development And Communications</option>
+                            <option value="Supervisor_de_Calidad_y_Gestion_Ambiental">VIP Lounges Junior Section Manager</option>
+                        </select>
+                    </div>
+                    <div class="input-group">
+                        <label for="cargo">Rol</label>
                         <select name="cargo" id="cargo">
                             <option value="">Seleccione una opción</option>
                             <option value="Administrador">Administrador</option>
-                            <option value="Supervisor">Supervisor</option>
-                            <option value="Empleado">Empleado</option>
-                        </select>                    
+                            <option value="Supervisor">Aprobador</option>
+                            <option value="Empleado">Digitador</option>
+                            <option value="Administrador">Observador</option>
+                         </select>                    
                     </div>
                     <div class="buttons">
                         <input type="submit" name="Submit" value="Registrarse" class="Registrarse">
