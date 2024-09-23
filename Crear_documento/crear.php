17f88fb1880aadb65c20f15fdb9b56bcd7ccd4f1
Thebeta1924 <miguelangelbeltranrache11@gmail.com>
2024-09-23 11:19:49 -0500

    actualizacion96

--- Crear_documento/crear.php
+++ Crear_documento/crear.php
@@ -70,16 +70,11 @@ $conn->close();
         <link rel="stylesheet" href="crear.css">
     </header>
     <li class="nav__item__user">
-<<<<<<< HEAD
         <a href="http://localhost/GateGourmet/Index/index_admin.html" class="cerrar__sesion__link">
             <img src="../Imagenes/regresar.png" alt="Usuario" class="img__usuario">
             <div class="cerrar__sesion">Volver al inicio</div>
         </a>
     </li>
-=======
-                <a href="http://localhost/GateGourmet/Index/index_admin.php" class="cerrar__sesion__link"><img src="../Imagenes/regresar.png" alt="Usuario" class="img__usuario"><div class="cerrar__sesion">Volver al inicio</div></a>
-            </li>
->>>>>>> 830c756f34f76d60d68e72cb1566acccfe4c2b48
     <main class="main-content">
         <div class="register-container">
             <div class="register-box">
